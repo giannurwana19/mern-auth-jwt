88881e5f0e85f75f722c5b4970d996c70a7627bd
giannurwana19 <giannurwana19@gmail.com>
2022-11-28 16:47:21 +0700

    backend - middleware verifyToken

---
+++ backend/src/middleware/verifyToken.js
@@ -0,0 +1,23 @@
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const accessToken = authHeader && authHeader.split(' ')[1];
+
+  if (!accessToken) {
+    return res.status(401).json({ success: false, message: 'Unauthorized!' });
+  }
+
+  jwt.verify(
+    accessToken,
+    process.env.APP_ACCESS_TOKEN_SECRET,
+    (err, decoded) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      req.username = decoded.username;
+      next();
+    }
+  );
+};
+
+module.exports = verifyToken;
--- backend/src/routes/indexRouter.js
+++ backend/src/routes/indexRouter.js
@@ -1,14 +1,38 @@
 const AuthController = require('../controllers/AuthController');
 const UserController = require('../controllers/UserController');
 
+const jwt = require('jsonwebtoken');
+const verifyToken = require('../middleware/verifyToken');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
   res.json({ message: 'hello world' });
 });
 
-router.get('/users', UserController.index);
+router.get('/users', verifyToken, UserController.index);
 router.get('/users/:id', UserController.show);
+
+router.get('/verify-token', (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const accessToken = authHeader && authHeader.split(' ')[1];
+
+  if (!accessToken) {
+    return res.status(401).json({ success: false, message: 'Unauthorized!' });
+  }
+
+  jwt.verify(
+    accessToken,
+    process.env.APP_ACCESS_TOKEN_SECRET,
+    (err, decoded) => {
+      if (err) {
+        return res.status(403).json({ success: false, message: 'Forbidden' });
+      }
+
+      req.username = decoded.username;
+      next();
+    }
+  );
+});
 
 router.post('/auth/register', AuthController.register);
 router.post('/auth/login', AuthController.login);
